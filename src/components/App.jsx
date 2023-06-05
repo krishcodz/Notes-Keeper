a33938721df23d06cd917530123813aacd9ce4d2
krishcodz <krishnax711@gmail.com>
2023-06-05 08:13:06 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setnotes] = React.useState([
+    { title: "test", content: "hello" }
+  ]);
+
+  function addNote(newNote) {
+    setnotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+  function deleteNote(id) {
+    setnotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea addNote={addNote} />
+      {console.log(notes)}
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            deleteNote={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
