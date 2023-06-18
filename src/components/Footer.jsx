3cef54fc160bafde741e13560e1fb84a2b12ffc9
krishna sai ram <74918581+krishcodz@users.noreply.github.com>
2023-06-18 05:30:05 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year} by <span><a href="https://krishcodz.github.io/personal_website/" target="_blank">Krishcodz</a></span></p>
+      <p>Copyright ⓒ {year} by <span><a href="https://krishcodz-portfolio.netlify.app/" target="_blank">Krishcodz</a></span></p>
     </footer>
   );
 }
